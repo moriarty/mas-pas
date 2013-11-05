61438aafd2046579944332868856e93669074034
moriarty <alexander@dal.ca>
2013-11-05 12:40:56 +0100

    Started HW3
    * This is for issue #1
    * I unzipped the sgp source into ~/src/sgp, please do the same
    * This shopping.lisp is only the example bomb in toilet problem from SGP readme

---
+++ hw3/shopping.lisp
@@ -0,0 +1,20 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Shopping problem using SGP
+;;;; Author: Alex Moriarty & Alex Hagg 
+;;;; Course: Planning & Scheduling
+;;;; Due: Tuesday, 5 November 2013, 1900h
+
+;;(defparameter *sgp-root-dir* (string "~/src/sgp/"))
+;;(load (concatenate 'string *sgp-root-dir* "loader"))
+
+(load "~/src/sgp/loader")
+(load-gp)
+(in-package :sgp)
+
+;;(defparameter *sgp-root-dir* (string "~/src/sgp/"))
+;;(load (concatenate 'string *sgp-root-dir* "domains/bt.lisp"))
+(load "~/src/sgp/domains/bt.lisp")
+
+(trace-gp contexts)
+(plan 'bt-2sa)
+(exit)
